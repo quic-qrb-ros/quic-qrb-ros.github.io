123e240ba32e91276d8517637738704e531bcd0f
zhaochen <zhaochen@codeaurora.org>
2024-08-20 18:17:58 +0800

    update favicon

--- source/conf.py
+++ source/conf.py
@@ -23,6 +23,7 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+html_favicon = 'resources/favicon.ico'
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 html_logo = "resources/qrb_ros_logo.png"
